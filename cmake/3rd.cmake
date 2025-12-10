92ef70f8764bf559af6af19fdd0447e244882670
chenyujin <3029795434@qq.com>
2025-12-10 19:50:53 +0800

    fix: 修改Cmake

--- cmake/3rd.cmake
+++ cmake/3rd.cmake
@@ -2,7 +2,7 @@
 # 设置导出静态库，这样install脚本中不需要顺带发布
 set(YAML_BUILD_SHARED_LIBS OFF CACHE BOOL "yamlCpp Build Shared Lib" FORCE) # 覆盖YAML的Option，生成静态库
 
-add_subdirectory(${CMAKE_SOURCE_DIR}/3rd/yaml-cpp-0.8.0)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rd/yaml-cpp-0.8.0)
 
 set_target_properties(yaml-cpp
         PROPERTIES
